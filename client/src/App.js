1094d7ce476cbf63f013376f72e2282ea23137ad
junko911 <junkotahara911@gmail.com>
2021-03-11 12:49:07 -0500

    Stylize form

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 function App() {
   return (
-    <>
+    <Container className="mt-2">
       <h1>Word Counter</h1>
-      <Form>
-        <FormGroup>
-          <Label for="url">URL</Label>
-          <Input
-            type="url"
-            name="url"
-            placeholder="https://www.example.com" />
-        </FormGroup>
-      </Form>
-      <Button>Submit</Button>
-    </>
+      <Row>
+        <Col>
+          <Form>
+            <FormGroup>
+              <Label for="url">URL</Label>
+              <Input
+                type="url"
+                name="url"
+                placeholder="https://www.example.com" />
+            </FormGroup>
+          </Form>
+          <Button>Submit</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
