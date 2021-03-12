a0a46871159ac9d467cfd1c8cd49ef9b489501a4
junko911 <junkotahara911@gmail.com>
2021-03-11 19:52:34 -0500

    Create count module

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 const app = express();
+const { count } = require('./count') 
 
 app.get('/', function(req, res) {
-  
+  const result = count()
+  res.send(result)
 });
 
 app.listen(3000);
