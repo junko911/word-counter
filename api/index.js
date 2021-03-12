6e7305fbf8d6d499113581d1da5622fff3350f3a
junko911 <junkotahara911@gmail.com>
2021-03-11 19:42:45 -0500

    Initiate index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var router = express.Router();
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res) {
+  
 });
 
-module.exports = router;
+app.listen(3000);
