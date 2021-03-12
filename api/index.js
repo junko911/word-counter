527f13e3f5743f7844eb1641696b69218ab6e504
junko911 <junkotahara911@gmail.com>
2021-03-11 22:25:53 -0500

    Create fetchUrlContent function

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 const app = express();
 const { count } = require('./count') 
+const { fetchUrlContent } = require('./fetch-url-content') 
+const url = "https://www.w3.org/TR/PNG/iso_8859-1.txt"
 
-app.get('/', function(req, res) {
-  const result = count("hello world")
+app.get('/', async function(req, res) {
+  const content = await fetchUrlContent(url)
+  const result = count(content)
   res.send(result)
 });
 
