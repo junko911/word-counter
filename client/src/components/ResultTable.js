c79ca99587e7891b9ae9ff6febff6810e75a7ff2
junko911 <junkotahara911@gmail.com>
2021-03-12 15:51:37 -0500

    Create result table

--- client/src/components/ResultTable.js
+++ client/src/components/ResultTable.js
@@ -1,96 +1,31 @@
 import React from 'react';
-import { useTable, useSortBy } from 'react-table';
 import { Table } from 'reactstrap'
 
 const ResultTable = ({ result }) => {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  )
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Word',
-        accessor: 'col1', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Count',
-        accessor: 'col2',
-      },
-    ],
-    []
-  )
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-    },
-    useSortBy
-  )
-
-  const firstPageRows = rows.slice(0, 20)
+  const genRows = words => {
+    return Object.keys(words).map(word => {
+      return(
+        <tr>
+          <td>{word}</td>
+          <td>{words[word]}</td>
+        </tr>
+      )
+    })
+  }
 
   return (
     <>
       <h2 className="mt-4">Result</h2>
-      <Table {...getTableProps()}>
+      <Table>
         <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                // Add the sorting props to control sorting. For this example
-                // we can add them into the header props
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  {/* Add a sort direction indicator */}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
+          <tr>
+            <th>Word</th>
+            <th>Count</th>
+          </tr>
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {firstPageRows.map(
-            (row, i) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    )
-                  })}
-                </tr>
-              )
-            }
-          )}
+        <tbody>
+          {genRows(result)}
         </tbody>
       </Table>
     </>
