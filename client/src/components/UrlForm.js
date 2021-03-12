17d649dce6e985afc4a29c5f486a9014709044ef
junko911 <junkotahara911@gmail.com>
2021-03-12 13:01:22 -0500

    Add submit function

--- client/src/components/UrlForm.js
+++ client/src/components/UrlForm.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
-const UrlForm = props => {
+const UrlForm = () => {
   const [url, changeUrl] = useState("")
 
   const submitHandler = e => {
     e.preventDefault()
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accepts: 'application/json',
+      },
+      body: JSON.stringify({
+        url: url
+      })
+    }
+    fetch('http://localhost:3000/', options)
+    .then(res => res.json())
+    .then(console.log)
   }
 
   return (
