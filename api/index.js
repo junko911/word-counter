395beefdaaeeaf4e813e1a1c15be820a248a1e30
junko911 <junkotahara911@gmail.com>
2021-03-12 12:29:25 -0500

    Create post route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
-const { count } = require('./count') 
-const { fetchUrlContent } = require('./fetch-url-content') 
-const { clean } = require('./clean')
-const url = "https://www.w3.org/TR/PNG/iso_8859-1.txt"
+const { count } = require('./count');
+const { fetchUrlContent } = require('./fetch-url-content');
+const { clean } = require('./clean');
 
-app.get('/', async function(req, res) {
-  const content = await fetchUrlContent(url)
-  const cleanedContent = clean(content)
-  const result = count(cleanedContent)
-  res.send(result)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', async function(req, res) {
+  const url = req.body.url;
+  const content = await fetchUrlContent(url);
+  const cleanedContent = clean(content);
+  const result = count(cleanedContent);
+  res.send(result);
 });
 
 app.listen(3000);
