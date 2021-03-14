784b199ff105b9bb285fd33ddab4df30cfd9b7a7
junko911 <junkotahara911@gmail.com>
2021-03-14 19:29:35 -0400

    Create clean test

--- api/test/count.js
+++ api/test/count.js
@@ -3,7 +3,7 @@ const { count } = require("../count");
 
 describe("Count", function () {
   it("counts words", function () {
-    const content = "word word word"
-    expect(count(content)).to.deep.equal({ word: 3 })
+    const content = "word word word";
+    expect(count(content)).to.deep.equal({ word: 3 });
   })
 })
