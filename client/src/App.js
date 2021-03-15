23ce9bc87c855ed53e9d616ee7ab9eeee56e5ca0
junko911 <junkotahara911@gmail.com>
2021-03-15 00:49:11 -0400

    Create submitHandler utility

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,19 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Container } from 'reactstrap';
-import UrlForm from './components/UrlForm';
-import ResultTable from './components/ResultTable';
+import React from "react"
+import "./App.css"
+import { Container } from "reactstrap"
+import UrlForm from "./components/UrlForm"
+import ResultTable from "./components/ResultTable"
+import { submitHandler } from "./Utils"
 
 function App() {
-  const submitUrl = url => {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accepts: 'application/json',
-      },
-      body: JSON.stringify({
-        url: url
-      })
-    }
-    fetch('http://localhost:3000/', options)
-    .then(res => res.json())
-    .then(data => {
-      let wordsDb = localStorage.getItem('wordsDb') || '{}'
-      let records = JSON.parse(wordsDb)
-
-      const date = Date.now()
-      records[date] = {url: url, result: data}
-      localStorage.setItem('wordsDb', JSON.stringify(records))
-    })
-  }
 
   return (
     <Container className="mt-2">
       <h1>Word Counter</h1>
-      <UrlForm submitUrl={submitUrl}/>
-      <ResultTable/>
+      <UrlForm submitUrl={submitHandler} />
+      <ResultTable />
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
