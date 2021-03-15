efc588c25bb1bd2222e6bc8e29589dacd50ff852
junko911 <junkotahara911@gmail.com>
2021-03-15 02:21:30 -0400

    Add coverage option

--- api/count.js
+++ api/count.js
@@ -1,11 +1,11 @@
 exports.count = function(string) {
-  let map = {};
-  const words = string.split(" ").filter( word => word !== "");
+  let map = {}
+  const words = string.split(" ").filter( word => word !== "")
 
   for(let i = 0; i < words.length; i++) {
-    const item = words[i];
-    map[item] = (map[item] + 1) || 1;
+    const item = words[i]
+    map[item] = (map[item] + 1) || 1
   }
 
-  return map;
+  return map
 }
