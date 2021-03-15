f5158275347d1181ecaf9d6504c73f539139911d
junko911 <junkotahara911@gmail.com>
2021-03-15 02:23:03 -0400

    Delete Utils.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,43 @@ import "./App.css"
 import { Container } from "reactstrap"
 import UrlForm from "./components/UrlForm"
 import ResultTable from "./components/ResultTable"
-import { submitHandler } from "./Utils"
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <Container className="mt-2">
-      <h1>Word Counter</h1>
-      <UrlForm submitUrl={submitHandler} />
-      <ResultTable />
-    </Container>
-  )
+  state = {
+    db: {}
+  }
+
+  submitUrl = url => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json",
+      },
+      body: JSON.stringify({
+        url: url,
+      }),
+    }
+    fetch("http://localhost:3000/", options)
+      .then(res => res.json())
+      .then(data => {
+        const newDb = this.state.db
+        const date = new Date().toLocaleString()
+        newDb[date] = { url: url, result: data }
+        this.setState({db: newDb})
+      })
+  }
+
+  render () {
+    return (
+      <Container className="mt-2">
+        <h1>Word Counter</h1>
+        <UrlForm submitUrl={this.submitUrl} />
+        <ResultTable db={this.state.db}/>
+      </Container>
+    )
+  }
 }
 
 export default App
